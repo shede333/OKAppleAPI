5298eb01bc80fc384478ec3c6a5d6f4ace379a49
shede333 <333wshw@163.com>
2022-01-01 23:40:01 +0800

    add OKProfileManager

--- ok_agent.py
+++ ok_agent.py
@@ -7,6 +7,13 @@ __author__ = 'shede333'
 from apple_api_agent import APIAgent, TokenManager
 
 
+class OKProfileManager:
+    """profile管理器"""
+
+    def __init__(self):
+        pass
+
+
 def main():
     pass
 
